f65b24157528fa93fe587bf8386275038cf22bac
Nathan Phipps <naphipps@pclnet.net>
2015-02-24 00:43:21 -0600

    Finally, something that looks uniform!

--- mode/mips.js
+++ mode/mips.js
@@ -13,24 +13,38 @@
 
 CodeMirror.defineMode('mips', function() {
     
-    //var DEFINITIONS = ".data";
+    var myDefinitions = /\.([248]?byte|a(ent|lias|lign|sciiz|scii|sm0)|b(gnb)|c(omm|app|padd|pload|plocal|prestore|preturn|psetup)|d(ata|ouble|word)|e(ndr|ndb|nd|nt|xtern|rr)|f(ile|loat|mask|rame)|g(jaldef|jallive|jrlive|lobl|pword)|h(alf)|k(data|text)|l(ab|comm|ivereg|oc)|m(ask)|n(ada|oalias|op)|o(ption|rigin)|r(epeat|data)|s(data|ection|et|ize|pace|truct)|t(ext|ype)|v(erstamp|reg)|w(eakext|ord))\b/i;
     
+    var myRegisters = /\$(a([0-3]|t)|f(p)|g(p)|k([0-1])|r(a)|s([0-8]|p)|t(\d)|v([0-1])|zero)\b/i;
     
+    var myKeywords = /\b(a(bs|ddiu|ddi|ddu|dd|ndi|nd)|b(czt|czf|eqz|eq|gezal|gez|geu|ge|gtu|gtz|gt|leu|lez|le|ltu|lt|nez|ltzal|ltz|ne|qez|)|c(lo|lz)|d(ivu|iv)|e(ret)|j(alr|al|r|)|l(a|bu|b|d|hu|h|i|ui|wcl|wl|wr|w)|m(addu|add|fc0|ove|sub|ulou|ulo|ult|ul|tc0)|n(egu|eg|omove|or|ot)|o(ri|r)|r(emu|em|ol|or)|s(b|c|dcl|d|eq|geu|ge|gtu|gt|h|leu|le|ne|llv|ll|ltiu|lti|ltu|lt|rav|ra|rlv|rl|ubu|ub|wcl|wr|wl|w|yscall)|t(eqi|eq|geu|geiu|gei|ge|ltu|ltiu|lti|lt)|u(lhu|lh|lw|sc|sh|sw)|x(ori|or))\b/i;
+    
+    var myNumbers = /\b([\da-f]+h|[0-7]+o|[0-1]+b|\d+)\b/i;
     
     return {
         startState: function(){
-            return {context: 0};      
-        
+            return {context: 0};        
         },
         token: function(stream, state){
             
-            var thisItem;
+            if (stream.eatSpace()){
+                return null;
+            }
             
-            thisItem = stream.current();
+            var thisItem;            
             
-            //if (stream.eatWhile(/\w/){
+            if (stream.eatWhile(/\w/)){
+                thisItem = stream.current();
                 
-            if (stream.eat('#')){
+                if (myNumbers.test(thisItem)){
+                    return 'number';
+                }
+                else if (myKeywords.test(thisItem)){
+                    return 'keyword';
+                }
+                
+            }
+            else if (stream.eat('#')){
                 stream.skipToEnd();
                 return 'comment';
             }
@@ -44,110 +58,42 @@ CodeMirror.defineMode('mips', function() {
                 return 'string';
                 
             }
-            else if (stream.eat('.')){//&& is a keyword definition
-                if(stream.eatWhile(/\w/)){
-                    return 'def';
+            else if (stream.eat('.')){
+                
+                if (stream.eatWhile(/\w/)){
+                    thisItem = stream.current();
+                    if (myDefinitions.test(thisItem)){
+                        return 'def';
+                    }
+                    return null;
+                    
                 }
+                
+                /*if(stream.eatWhile(/\w/)){
+                    return 'def';
+                }//*/
             }
-            else {            
+            else if(stream.eat('$')){
+                
+                if(stream.eatWhile(/\w/)){
+                    thisItem = stream.current();
+                    
+                    if (myRegisters.test(thisItem)){
+                        return 'atom';
+                    }                    
+                }
+                
+            }
+            else {
                 stream.next();
             }
             
             return null;
             
-            
-            
-            
         }
     };
-    //*/
-    /*
-  var keywords1 = /^(exx?|(ld|cp|in)([di]r?)?|pop|push|ad[cd]|cpl|daa|dec|inc|neg|sbc|sub|and|bit|[cs]cf|x?or|res|set|r[lr]c?a?|r[lr]d|s[lr]a|srl|djnz|nop|rst|[de]i|halt|im|ot[di]r|out[di]?)\b/i;
-  var keywords2 = /^(call|j[pr]|ret[in]?)\b/i;
-  var keywords3 = /^b_?(call|jump)\b/i;
-  var variables1 = /^(af?|bc?|c|de?|e|hl?|l|i[xy]?|r|sp)\b/i;
-  var variables2 = /^(n?[zc]|p[oe]?|m)\b/i;
-  var errors = /^([hl][xy]|i[xy][hl]|slia|sll)\b/i;
-  var numbers = /^([\da-f]+h|[0-7]+o|[01]+b|\d+)\b/i;
-
-  return {
-    startState: function() {
-      return {context: 0};
-    },
-    token: function(stream, state) {
-      if (!stream.column())
-        state.context = 0;
-
-      if (stream.eatSpace())
-        return null;
-
-      var w;
-
-      if (stream.eatWhile(/\w/)) {
-        w = stream.current();
-
-        if (stream.indentation()) {
-          if (state.context == 1 && variables1.test(w))
-            return 'variable-2';
-
-          if (state.context == 2 && variables2.test(w))
-            return 'variable-3';
-
-          if (keywords1.test(w)) {
-            state.context = 1;
-            return 'keyword';
-          } else if (keywords2.test(w)) {
-            state.context = 2;
-            return 'keyword';
-          } else if (keywords3.test(w)) {
-            state.context = 3;
-            return 'keyword';
-          }
-
-          if (errors.test(w))
-            return 'error';
-        } else if (numbers.test(w)) {
-          return 'number';
-        } else {
-          return null;
-        }
-      } else if (stream.eat('#')) {
-        stream.skipToEnd();
-        return 'comment';
-      } else if (stream.eat('"')) {
-        while (w = stream.next()) {
-          if (w == '"')
-            break;
-
-          if (w == '\\')
-            stream.next();
-        }
-        return 'string';
-      } else if (stream.eat('\'')) {
-        if (stream.match(/\\?.'/))
-          return 'number';
-      } else if (stream.eat('.') || stream.sol() && stream.eat('#')) {
-        state.context = 4;
-
-        if (stream.eatWhile(/\w/))
-          return 'def';
-      } else if (stream.eat('$')) {
-        if (stream.eatWhile(/[\da-f]/i))
-          return 'number';
-      } else if (stream.eat('%')) {
-        if (stream.eatWhile(/[01]/))
-          return 'number';
-      } else {
-        stream.next();
-      }
-      return null;
-    }
-  };
     
-    //*/
 });
-
-//CodeMirror.defineMIME("text/mips", "mips");
-
+    
 });
 
