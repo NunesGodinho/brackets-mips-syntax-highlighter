c0ad364c9eac9a1b903927cbc804bc0379ff0140
Nathan Phipps <naphipps@pclnet.net>
2016-02-04 23:04:56 -0600

    fixed everything

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-define(function (require, exports, module) {"use strict";var LanguageManager = brackets.getModule("language/LanguageManager");LanguageManager.defineLanguage("mips", {name: "MIPS",mode: "mips",fileExtensions: ["s","sx"],lineComment: ["#"]});});
+define(function (require, exports, module) {"use strict";var LanguageManager = brackets.getModule("language/LanguageManager");LanguageManager.defineLanguage("mips", {name: "mips",mode: "mips",fileExtensions: ["s","sx"],lineComment: ["#"]});});
