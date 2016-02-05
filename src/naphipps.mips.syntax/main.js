f6743e8452064827f9e4378c36071880641ce3c7
Nathan Phipps <naphipps@pclnet.net>
2016-02-05 00:26:43 -0600

    update

--- src/naphipps.mips.syntax/main.js
+++
@@ -1,1 +0,0 @@
-define(function (require, exports, module) {"use strict";var LanguageManager = brackets.getModule("language/LanguageManager");LanguageManager.defineLanguage("mips", {name: "MIPS",mode: "mips",fileExtensions: ["s","sx"],lineComment: ["#"]});});
