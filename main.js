df9af134054574883dc69fb9ccfa539ad24aff79
Nathan Phipps <naphipps@pclnet.net>
2016-02-04 22:39:41 -0600

    copied main.js and package.json to home dir, and added zip

---
+++ main.js
@@ -0,0 +1,1 @@
+define(function (require, exports, module) {"use strict";var LanguageManager = brackets.getModule("language/LanguageManager");LanguageManager.defineLanguage("mips", {name: "MIPS",mode: "mips",fileExtensions: ["s","sx"],lineComment: ["#"]});});
