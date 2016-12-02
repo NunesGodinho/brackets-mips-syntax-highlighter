33fdb2889912a8aac5bae13167f1f791dddac28f
NunesGodinho <nunes.godinho@gmail.com>
2016-12-02 10:28:59 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -2,19 +2,19 @@ define(function (require, exports, module) {
     "use strict";
     var LanguageManager = brackets.getModule("language/LanguageManager");
     
-    LanguageManager.defineLanguage("mips", {
-        name: "mips",
-        mode: "mips",
+    LanguageManager.defineLanguage("mips64", {
+        name: "mips64",
+        mode: "mips64",
         fileExtensions: ["s", "sx"],
         lineComment: ["#"]
     });
 });
 
-//************************************************
-//      CodeMirror MIPS Mode
-//          Author: Nathan Phipps
-//          Site: github.com/naphipps
-//************************************************
+//******************************************************************************************
+//      CodeMirror MIPS Mode for EDU MIPS 64
+//          Author: António Godinho, based on the original code from Nathan Phipps
+//          Site: https://github.com/NunesGodinho
+//******************************************************************************************
 
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: http://codemirror.net/LICENSE
@@ -32,13 +32,13 @@ define(function (require, exports, module) {
 "use strict";
 */
 
-CodeMirror.defineMode('mips', function() {
+CodeMirror.defineMode('mips64', function() {
     
     var myDefinitions = /\.([248]?byte|a(ent|lias|lign|sciiz|scii|sm0)|b(gnb)|c(omm|app|padd|pload|plocal|prestore|preturn|psetup)|d(ata|ouble|word)|e(ndr|ndb|nd|nt|xtern|rr)|f(ile|loat|mask|rame)|g(jaldef|jallive|jrlive|lobl|pword)|h(alf)|k(data|text)|l(ab|comm|ivereg|oc)|m(ask)|n(ada|oalias|op)|o(ption|rigin)|r(epeat|data)|s(data|ection|et|ize|pace|truct)|t(ext|ype)|v(erstamp|reg)|w(eakext|ord))\b/i;
     
-    var myRegisters = /\$(a([0-3]|t)|f(p)|g(p)|k([0-1])|r(0|a)|s([0-8]|p)|t(\d)|v([0-1])|zero)\b/i;
+    var myRegisters = /\r([0-31])\b/i;
     
-    var myKeywords = /\b(a(bs|ddiu|ddi|ddu|dd|ndi|nd)|b(czt|czf|eqz|eq|gezal|gez|geu|ge|gtu|gtz|gt|leu|lez|le|ltu|lt|nez|ltzal|ltz|ne|qez|)|c(lo|lz)|d(ivu|iv)|e(ret)|j(alr|al|r|)|l(a|bu|b|d|hu|h|i|ui|wcl|wl|wr|w)|m(addu|add|fc0|flo|fhi|ove|sub|ulou|ulo|ult|ul|tc0)|n(egu|eg|omove|or|ot)|o(ri|r)|r(emu|em|ol|or)|s(b|c|dcl|d|eq|geu|ge|gtu|gt|h|leu|le|ne|llv|ll|ltiu|lti|ltu|lt|rav|ra|rlv|rl|ubu|ub|wcl|wr|wl|w|yscall)|t(eqi|eq|geu|geiu|gei|ge|ltu|ltiu|lti|lt)|u(lhu|lh|lw|sc|sh|sw)|x(ori|or))\b/i;
+    var myKeywords = /\b(a(bs|ddiu|ddi|ddu|dd|ndi|nd)|b(czt|czf|eqz|eq|gezal|gez|geu|ge|gtu|gtz|gt|leu|lez|le|ltu|lt|nez|ltzal|ltz|ne|qez|)|c(lo|lz)|d(ivu|div|add|addi|mult)|e(ret)|j(alr|al|r|)|l(a|bu|b|d|hu|h|i|ui|wcl|wl|wr|w)|m(addu|add|fc0|flo|fhi|ove|sub|ulou|ulo|ult|ul|tc0)|n(egu|eg|omove|or|ot)|o(ri|r)|r(emu|em|ol|or)|s(b|c|dcl|d|eq|geu|ge|gtu|gt|h|leu|le|ne|llv|ll|ltiu|lti|ltu|lt|rav|ra|rlv|rl|ubu|ub|wcl|wr|wl|w|yscall)|t(eqi|eq|geu|geiu|gei|ge|ltu|ltiu|lti|lt)|u(lhu|lh|lw|sc|sh|sw)|x(ori|or))\b/i;
     
     var myNumbers = /\b([\da-f]+h|[0-7]+o|[0-1]+b|\d+)\b/i;
     
